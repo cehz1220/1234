bf356c695a92a8768bde328492f17c90311ec2fb
Студент Студент <student@mac205-03.ouiit.local>
2024-12-12 14:43:27 +0300

    aaaaaaa

--- app/src/main/java/com/example/myapplication1/ui/activity/SprintActivity1.kt
+++ app/src/main/java/com/example/myapplication1/ui/activity/SprintActivity1.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication1.R
 
-class MainActivity2 : AppCompatActivity() {
+class SprintActivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
